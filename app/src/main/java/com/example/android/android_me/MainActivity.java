7e266e783d8b2b9078016c5ceb3517300e39dbf6
Sudhanshu Kulshrestha <48475411+sudkul@users.noreply.github.com>
2022-06-28 13:10:07 +0530

    Add archival note to README

--- app/src/main/java/com/example/android/android_me/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.android.android_me;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
